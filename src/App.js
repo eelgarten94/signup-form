3ce711ac17def692a48764907b92359aa5c5a34d
elias.elgarten@gmail.com <eliaselgarten@eliass-mbp.myfiosgateway.com>
2024-03-06 17:21:56 -0500

    Add signup form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>User Signup Form</h1>
       </header>
+      <SignupForm />
     </div>
   );
 }
 
 export default App;
+
+const SignupForm = () => {
+  const [email, setEmail] = React.useState('')
+  const [password, setPassword] = React.useState('')
+  const [confirmPassword, setConfirmPassword] = React.useState('')
+
+  const [resultText, setResultText] = React.useState('') // Displays the result to the user
+
+  const handleSubmit = React.useCallback((e) => {
+    e.preventDefault() // Prevent page from reloading on submit
+
+    if (!(email && password && confirmPassword)) setResultText('Please fill in all form fields.')
+    else if (password === confirmPassword) setResultText('Passwords match!')
+    else if (password !== confirmPassword) setResultText('Passwords do not match.')
+  }, [email, password, confirmPassword])
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <form onSubmit={handleSubmit} className='signup-form'>
+        <div className='input-field'> <label id="email">Email</label><input value={email} name='email' type='text' onChange={(e) => setEmail(e.target.value)} /></div>
+        <div className='input-field'><label id="password">Password</label><input value={password} name='password' onChange={(e) => setPassword(e.target.value)} /></div>
+        <div className='input-field'><label id="confirm-password">Confirm Password</label><input value={confirmPassword} name='confirm-password' onChange={(e) => setConfirmPassword(e.target.value)} /></div>
+        <button type="submit">Submit</button>
+      </form>
+
+      <p>{resultText}</p>
+    </div>
+
+  )
+}
