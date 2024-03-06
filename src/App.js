43c32ad19b22b1c37f7e11c4722894cb059cafd5
elias.elgarten@gmail.com <eliaselgarten@eliass-mbp.myfiosgateway.com>
2024-03-06 17:29:57 -0500

    Change email to username

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 }
 
 const SignupForm = () => {
-  const [email, setEmail] = React.useState('')
+  const [username, setUsername] = React.useState('')
   const [password, setPassword] = React.useState('')
   const [confirmPassword, setConfirmPassword] = React.useState('')
 
@@ -22,15 +22,15 @@ const SignupForm = () => {
   const handleSubmit = React.useCallback((e) => {
     e.preventDefault() // Prevent page from reloading on submit
 
-    if (!(email && password && confirmPassword)) setResultText('Please fill in all form fields.')
+    if (!(username && password && confirmPassword)) setResultText('Please fill in all form fields.')
     else if (password === confirmPassword) setResultText('Passwords match!')
     else if (password !== confirmPassword) setResultText('Passwords do not match.')
-  }, [email, password, confirmPassword])
+  }, [username, password, confirmPassword])
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       <form onSubmit={handleSubmit} className='signup-form'>
-        <div className='input-field'> <label id="email">Email</label><input value={email} name='email' type='text' onChange={(e) => setEmail(e.target.value)} /></div>
+        <div className='input-field'> <label id="email">Username</label><input value={username} name='username' type='text' onChange={(e) => setUsername(e.target.value)} /></div>
         <div className='input-field'><label id="password">Password</label><input value={password} name='password' onChange={(e) => setPassword(e.target.value)} /></div>
         <div className='input-field'><label id="confirm-password">Confirm Password</label><input value={confirmPassword} name='confirm-password' onChange={(e) => setConfirmPassword(e.target.value)} /></div>
         <button type="submit">Submit</button>
